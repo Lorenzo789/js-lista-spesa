b1474f585a213bfc1f44489793bd819b32e5045b
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-06-21 14:54:10 +0200

    ciclo for to help me

--- js/myscript.js
+++ js/myscript.js
@@ -1,2 +1,48 @@
 
 
+const listItemStore = [
+    'coffee',
+    'milk',
+    'bread',
+    'orange',
+    'apple',
+    'spinach',
+    'peas',
+    'pasta',
+    'venus rice'
+]
+
+//inserire tot elementi li nell'html
+//creare un ciclo for 
+//farlo con un ciclo while
+
+const cartItemShop = document.getElementById('list-shop');
+
+
+// let index = 0;
+
+// while (index < listItemStore.length) {
+    
+    //     cartItemShop.append(newItem);
+    
+    //     index++;
+    // 
+}
+    
+
+
+
+
+
+
+// ! CICLO FOR ! \\
+
+// for (let index = 0; index < listItemStore.length; index++) {
+        
+//     const newItem = document.createElement('li');
+
+//     cartItemShop.append(newItem);
+
+//     newItem.innerHTML = (listItemStore[index]);
+
+// }
